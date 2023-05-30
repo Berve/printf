c73f279c7477f0a751b03e05d55fdcfeaafd0c96
Berve <bervely.vanderpuye@gmail.com>
2023-05-30 19:17:20 +0000

    printf

--- _printf.c
+++ _printf.c
@@ -53,12 +53,12 @@ int _printf(const char *format, ...)
 	return (printed_chars);
 	}
 
-	/**
-	 * print_buffer: prints the contents of the buffer if it exist
-	 * @buffer: Array of chars
-	 *
-	 * @buff_ind: index at which to add next char,represents the length.
-	 */
+/**
+ * print_buffer - prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ *
+ * @buff_ind: index at which to add next char,represents the length.
+ */
 
 void print_buffer(char buffer[], int *buff_ind)
 	{
